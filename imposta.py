99e31e48f898c79455713dea76fca79bb939fb29
davide sambughi <davidesambughi@gmail.com>
2025-04-01 13:03:30 +0200

    imposta forse corretto?

---
+++ imposta.py
@@ -0,0 +1,28 @@
+
+reddito= int(input ("inserisci il tuo reddito: "))
+aliquota1= 0.23
+aliquota2=0.35
+aliquota3=0.43
+
+SCAGLIONE1=28000
+SCAGLIONE2=50000
+
+VA1=int (6440)
+VA2= int (7700)
+imposta=0
+importo=0
+
+
+if reddito <= SCAGLIONE1 :
+    imposta= (reddito* aliquota1)
+    print ("devi pagare " , imposta)
+else :
+    if reddito <=SCAGLIONE2 and reddito> SCAGLIONE1:
+        imposta= (reddito - SCAGLIONE1)* aliquota2
+        importo= imposta + VA1
+        print ( "devi pagare " , importo)
+    else : 
+     if reddito > SCAGLIONE2 :
+        imposta = (reddito - SCAGLIONE2 )*aliquota3
+        importo=  imposta + VA2
+     print (  "devi pagare " , importo )
