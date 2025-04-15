97f3777938325b584b872250ebe75c825a6db901
davide sambughi <davidesambughi@gmail.com>
2025-04-15 15:00:10 +0200

    lettera (copia Andrea)

---
+++ lettera.py
@@ -0,0 +1,25 @@
+class Letter:
+    def __init__(self,letterFrom,letterTo):
+        self._letterFrom = letterFrom 
+        self._letterTo   = letterTo
+        self.line        = ""
+
+    def addLine(self,line):
+        self.line+= "\n" + line
+       
+        
+        
+
+    def getText (self):
+       return f" Dear { self._letterTo} : \n"+ self.line + f" \n\nSincerely, \n\n {self._letterFrom}"
+    
+
+missiva = Letter("Mary", "john")
+missiva.addLine (" I am sorry we must part")
+missiva.addLine (" I wish you all the best")
+
+
+
+print(missiva.getText())
+
+     
